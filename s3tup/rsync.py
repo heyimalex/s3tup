545eba83000df20340885ae59339ff63ad35850c
Alex N Guerra <alex@heyimalex.com>
2013-09-21 17:12:55 -0500

    Add rsync_key method to Rsync to replace _get_local_file.
    Ultimately this makes a lot more sense than how it was previously
    done; now Rsync objects can be used on buckets as a whole or on
    individual keys. Also removed some extraneous references to protect
    feature that's not yet implemented and fleshed out some docstrings.

--- s3tup/rsync.py
+++ s3tup/rsync.py
@@ -6,13 +6,11 @@ import s3tup.utils as utils
 log = logging.getLogger('s3tup.rsync')
 
 class Rsync(object):
-    def __init__(self, src=None, dest=None, delete=False, matcher=None,
-                 protect=None):
+    def __init__(self, src=None, dest=None, delete=False, matcher=None):
         self.src = src
         self.dest = dest
         self.delete = delete
         self.matcher = matcher
-        self.protect = protect
 
     def run(self, bucket, protect=None):
         # Silence key logging to avoid redundant messages
@@ -26,11 +24,6 @@ class Rsync(object):
         log.info("rsyncing '{}'".format(os.path.join(bucket_url, dest)))
         log.info("    with '{}'...".format(os.path.abspath(src)))
 
-        if protect is not None:
-            protect += self.protect
-        else:
-            protect = self.protect
-
         local_keys = self._get_local_keys()
         remote_keys = list(bucket.get_remote_keys(prefix=self.dest))
 
@@ -41,8 +34,8 @@ class Rsync(object):
         modified_keys = keys['modified']
         unmodified_keys = keys['unmodified']
 
-        # Delete removed keys if delete
-        if self.delete is True:
+        # Delete removed keys if delete is True
+        if self.delete:
             bucket.delete_remote_keys(removed_keys)
         else:
             unmodified_keys.extend(removed_keys)
@@ -52,13 +45,13 @@ class Rsync(object):
         for k in new_keys:
             key = bucket.make_key(k)
             log.info("new: '{}', uploading now...".format(k))
-            key.rsync(self._get_local_file(k))
+            self.rsync_key(key)
 
         # Upload modified keys
         for k in modified_keys:
             key = bucket.make_key(k)
             log.info("modified: '{}', uploading now...".format(k))
-            key.rsync(self._get_local_file(k))
+            self.rsync_key(key)
 
         # Unsilence key logging
         key_log.setLevel(logging.DEBUG)
@@ -74,11 +67,18 @@ class Rsync(object):
                 'unmodified': unmodified_keys}
 
     def _get_local_keys(self):
+        """Return bucket.get_remote_keys-like response from files in self.src
+
+        Uses utils.os_walk_relative to get all filenames relative to
+        self.src, and then returns a list of dicts similar to
+        Bucket.get_remote_keys. This value is used as an input to key_diff.
+
+        """
         src = self.src or '.'
         dest = self.dest or ''
         matcher = self.matcher or utils.Matcher()
         keys = []
-        for relpath in utils.os_walk_iter(src):
+        for relpath in utils.os_walk_relative(src):
             if not matcher.matches(relpath):
                 continue
 
@@ -92,8 +92,16 @@ class Rsync(object):
             keys.append(key)
         return keys
 
-    def _get_local_file(self, key_name):
+    def rsync_key(self, key):
+        """Rsyncs s3tup.key.Key 'key' with its corresponding local file.
+
+        Takes a s3tup.key.Key object and rsyncs it with its corresponding
+        local file. The local file path is basically just the key name minus
+        self.dest relative to self.src.
+
+        """
         src = self.src or ''
         dest = self.dest or ''
-        unrelative = os.path.relpath(key_name, dest)
-        return open(os.path.join(src, unrelative), 'rb')
+        unrelative = os.path.relpath(key.name, dest)
+        local_path = os.path.join(src, unrelative)
+        key.rsync(open(local_path, 'rb'))
